b6675309e93563c7471f1ca5a85ae400377f3d63
Charlie Xiao <xiaoguangxuan@outlook.com>
2020-03-01 03:32:32 +0800

    group TODO completed

--- PA1/student-code/include/group.hpp
+++ PA1/student-code/include/group.hpp
@@ -1,47 +1,38 @@
 #ifndef GROUP_H
 #define GROUP_H
 
-
-#include "object3d.hpp"
-#include "ray.hpp"
-#include "hit.hpp"
 #include <iostream>
 #include <vector>
+
+#include "hit.hpp"
+#include "object3d.hpp"
+#include "ray.hpp"
 
 // 存储物体列表：球、平面，组织成一个列表
 // TODO: Implement Group - add data structure to store a list of Object*
 class Group : public Object3D {
+   public:
+    Group() {}
 
-public:
+    explicit Group(int num_objects) : objList(num_objects) {}
 
-    Group() {
-
-    }
-
-    explicit Group (int num_objects) {
-
-    }
-
-    ~Group() override {
-
-    }
+    ~Group() override {}
 
     // 对列表里所有物体都求一遍交点
     bool intersect(const Ray &r, Hit &h, float tmin) override {
-
+        bool flag = false;
+        for (auto obj : objList) flag = flag || obj->intersect(r, h, tmin);
+        return flag;
     }
 
     void addObject(int index, Object3D *obj) {
-
+        objList.insert(objList.begin() + index, obj);
     }
 
-    int getGroupSize() {
+    int getGroupSize() { return objList.size(); }
 
-    }
-
-private:
-
+   private:
+    std::vector<Object3D *> objList;
 };
 
 #endif
-	
